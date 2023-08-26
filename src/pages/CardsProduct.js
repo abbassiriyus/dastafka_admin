408edd1aeee87b29a47daaabbe990558a53ba046
Akbar-07 <86982819+abbassiriyus@users.noreply.github.com>
2023-08-26 09:45:55 +0300

    sfd
    sdf

--- src/pages/CardsProduct.js
+++ src/pages/CardsProduct.js
@@ -1,46 +1,168 @@
-import React from 'react';
-import { Card, Col, Row } from 'antd';
+import React, { useEffect, useState } from 'react'
+import { message, Button, Card, Checkbox, Col, Input, Modal, Row, Image } from 'antd';
 import "./stylee.css";
+import url from './host.js';
+import axios from 'axios';
+const { TextArea } = Input;
+export default function CardsProduct() {
+  var [category, setCategory] = useState([])
+  var [checkFile, setCheckFile] = useState(false)
 
-const App: React.FC = () => (
-  <Row gutter={18}>
-    <Col span={6}>  
-    <div className="imgNameFour">
-      <Card  bordered={false} >
-      <div className="tickCircle"></div>
-      <center><img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg" alt="" className='imgProductFour'/>
-      <h4>name</h4></center>
-      </Card>
-      </div>
-    </Col>  
-    <Col span={6}>  
-    <div className="imgNameFour">
-      <Card  bordered={false} >
-      <div className="tickCircle"></div>
-      <center><img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg" alt="" className='imgProductFour'/>
-      <h4>name</h4></center>
-      </Card>
-      </div>
-    </Col>   
-    <Col span={6}>  
-    <div className="imgNameFour">
-      <Card  bordered={false} >
-      <div className="tickCircle"></div>
-      <center><img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg" alt="" className='imgProductFour'/>
-      <h4>name</h4></center>
-      </Card>
-      </div>
-    </Col>  
-    <Col span={6}>  
-    <div className="imgNameFour">
-      <Card  bordered={false} >
-      <div className="tickCircle"></div>
-      <center><img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg" alt="" className='imgProductFour'/>
-      <h4>name</h4></center>
-      </Card>
-      </div>
-    </Col>  
-  </Row>
-);
 
-export default App;
+  var [data, setdata] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen1, setIsModalOpen1] = useState(false);
+const [deleteid,setDeleteId]=useState(-1)
+  const showModal1 = () => {
+    setIsModalOpen1(true);
+  };
+
+  const handleOk1 = () => {
+    setIsModalOpen1(false); 
+    axios.delete(`${url}/api/category/${deleteid}`).then(res=>{
+    message.success('deleted');
+    axios.get(`${url}/api/category`).then(res => {
+      var a = res.data
+      for (let i = 0; i < a.length; i++) {
+        if (i == 0) {
+          a[i].check = true
+        } else {
+          a[i].check = false
+        }
+      }
+      setCategory(res.data)
+    })
+  }).catch(err=>{
+    message.error("don't delete")
+  })
+  };
+
+  const handleCancel1 = () => {
+    setIsModalOpen1(false);
+  };
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    
+  };
+  
+function setCheck(key) {
+for (let i = 0; i < category.length; i++) {
+ if (key==i) {
+ document.querySelectorAll('.tickCircle')[key].innerHTML=`<i class='bx bx-check'></i>` 
+ document.querySelectorAll('.ant-card-body')[key].style=`border:2px solid orange;border-radius:10px` 
+ }else{
+  document.querySelectorAll('.tickCircle')[i].innerHTML=`<div></div>` 
+  document.querySelectorAll('.ant-card-body')[i].style=`border: none` 
+}}}
+function postData(){
+  var data=new FormData()
+  data.append("title",document.querySelector("#title").value)
+  if(checkFile){
+  data.append("image",document.querySelector("#file").files[0])
+  }else{
+  data.append("image",document.querySelector("#file").value)
+  }
+  data.append("description",document.querySelector("#deck").value)
+axios.post(`${url}/api/category`,data).then(res=>{
+  message.success(" Created ")
+  axios.get(`${url}/api/category`).then(res => {
+    var a = res.data
+    for (let i = 0; i < a.length; i++) {
+      if (i == 0) {
+        a[i].check = true
+      } else {
+        a[i].check = false
+      }
+
+    }
+    setCategory(res.data)
+
+  })
+  setIsModalOpen(false);
+}).catch(err=>{
+ message.error("don't create")
+  setIsModalOpen(false);
+})
+}
+function onFile(e){
+ setCheckFile(e.target.checked)
+  if(e.target.checked){
+  document.querySelector("#file").type="file"
+  }else{
+    document.querySelector("#file").type="text"
+  }
+}
+function deteteData(params) {
+setDeleteId(params)
+showModal1()
+}
+  useEffect(() => {
+    axios.get(`${url}/api/category`).then(res => {
+      var a = res.data
+      for (let i = 0; i < a.length; i++) {
+        if (i == 0) {
+          a[i].check = true
+        } else {
+          a[i].check = false
+        }
+
+      }
+      setCategory(res.data)
+
+    })
+  }, [])
+
+  return (
+    <div>
+     
+     <Button type="primary" onClick={()=>showModal()}>
+       Create Category
+      </Button>
+      <Modal title="Basic Modal" visible={isModalOpen} onOk={()=>postData()} onCancel={()=>handleCancel()}>
+      <Input id='title' showCount maxLength={50} placeholder='title'  />
+    <br />
+    <br />
+    <Checkbox onChange={(e)=>onFile(e)}>file</Checkbox>
+    <Input type='text' id='file' placeholder='image'  />
+    <br />
+    <br />
+    <TextArea showCount id='deck' maxLength={400}  placeholder='deckription' />
+      </Modal>
+
+      {category.length === 0 ? (<div>no category beton</div>) : (<Row gutter={18}>{category.map((item, key) => {
+        return <Col onClick={() => { setCheck(key) }} span={4}>
+          <div className="imgNameFour">
+            <Card bordered={false} >
+              
+              <div className="tickCircle">{item.check?(<i class='bx bx-check'></i>):(<div></div>)}</div>
+              <center><Image width={'90%'} src={item.image.includes("http") ? item.image : `${url}/${item.image}`} alt="no image" className='imgProductFour' />
+                <h4>{item.title}</h4>
+                <p>{item.description}</p>
+                <div className="icons"  style={{fontSize:'23px',display:'flex',justifyContent:'space-around'}}><i class='bx bxs-trash' style={{cursor:'pointer'}} onClick={()=>{deteteData(item.id)}} ></i><i class='bx bx-edit' ></i></div>
+                </center>
+            </Card>
+          </div>
+        </Col>
+      })} </Row>)}
+
+
+      <Modal title="Осторожность" visible={isModalOpen1} onOk={handleOk1} onCancel={handleCancel1}>
+    <p>Вы уверены, что хотите удалить эту информацию? Это может привести к плохим последствиям.</p>
+      </Modal>
+      
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,14 +1,3 @@
-/*!
-=========================================================
-* Muse Ant Design Dashboard - v1.0.0
-=========================================================
-* Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
-* Coded by Creative Tim
-=========================================================
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 import {
   Row,
   Col,
@@ -20,8 +9,6 @@ import {
   Typography,
 } from "antd";
 
-
-// Images
 import face from "../assets/images/face-1.jpg";
 import face2 from "../assets/images/face-2.jpg";
 import face3 from "../assets/images/face-3.jpg";
@@ -30,6 +17,9 @@ import face5 from "../assets/images/face-5.jpeg";
 import face6 from "../assets/images/face-6.jpeg";
 import CardsProduct from "./CardsProduct.js";
 import "./style.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import url from "./host";
 const { Title } = Typography;
 
 const formProps = {
@@ -49,7 +39,21 @@ const formProps = {
     }
   },
 };
-// table code start
+
+
+function Tables() {
+
+var [data, setdata]=useState([])
+
+
+  const onChange = (e) => console.log(`radio checked:${e.target.value}`);
+
+
+
+
+
+
+
 const columns = [
   {
     title: "Производитель",
@@ -75,229 +79,11 @@ const columns = [
   },
 ];
 
-const data = [
-  {
-    key: "1",
-    name: (
-      <>
-        <Avatar.Group>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJDCfJoArPLNaU2wiv2NcQPvocrWJCV42aBK8LFqy-ADnU3VxdkqBZ9VOCvRyCFjkiu0&usqp=CAU" alt="" className="someone" />
-        </Avatar.Group>{" "}
-      </>
-    ),
-    function: (
-      <>
-        <div className="author-info">
-          <Title level={5}>M200</Title>
- 
-        </div>
-      </>
-    ),
-
-    status: (
-      <>
-              Наименование опции
-      </>
-    ),
-    employed: (
-      <>
-        <div className="ant-employed">
-          <span>2 000</span>
-          <div className="aProduct">
-          <a href="#pablo">Edit</a>
-          <a href="#pablo" style={{"color":'red'}}>Delete</a>
-          </div>
-        </div>
-      </>
-    ),
-  },
-
-  {
-    key: "2",
-    name: (
-      <>
-        <Avatar.Group>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJDCfJoArPLNaU2wiv2NcQPvocrWJCV42aBK8LFqy-ADnU3VxdkqBZ9VOCvRyCFjkiu0&usqp=CAU" alt="" className="someone" />
-        </Avatar.Group>{" "}
-      </>
-    ),
-    function: (
-      <>
-        <div className="author-info">
-          <Title level={5}>M200</Title>
- 
-        </div>
-      </>
-    ),
-
-    status: (
-      <>
-      Наименование опции
-      </>
-    ),
-    employed: (
-      <>
-        <div className="ant-employed">
-          <span>2 000</span>
-          <div className="aProduct">
-          <a href="#pablo">Edit</a>
-          <a href="#pablo" style={{"color":'red'}}>Delete</a>
-          </div>
-        </div>
-      </>
-    ),
-  },
-
-  {
-    key: "3",
-    name: (
-      <>
-        <Avatar.Group>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJDCfJoArPLNaU2wiv2NcQPvocrWJCV42aBK8LFqy-ADnU3VxdkqBZ9VOCvRyCFjkiu0&usqp=CAU" alt="" className="someone" />
-        </Avatar.Group>{" "}
-      </>
-    ),
-    function: (
-      <>
-        <div className="author-info">
-          <Title level={5}>M200</Title>
- 
-        </div>
-      </>
-    ),
-
-    status: (
-      <>
-              Наименование опции
-      </>
-    ),
-    employed: (
-      <>
-        <div className="ant-employed">
-          <span>2 000</span>
-          <div className="aProduct">
-          <a href="#pablo">Edit</a>
-          <a href="#pablo" style={{"color":'red'}}>Delete</a>
-          </div>
-        </div>
-      </>
-    ),
-  },
-  {
-    key: "4",
-    name: (
-      <>
-        <Avatar.Group>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJDCfJoArPLNaU2wiv2NcQPvocrWJCV42aBK8LFqy-ADnU3VxdkqBZ9VOCvRyCFjkiu0&usqp=CAU" alt="" className="someone" />
-        </Avatar.Group>{" "}
-      </>
-    ),
-    function: (
-      <>
-        <div className="author-info">
-          <Title level={5}>M200</Title>
- 
-        </div>
-      </>
-    ),
-
-    status: (
-      <>
-              Наименование опции
-      </>
-    ),
-    employed: (
-      <>
-        <div className="ant-employed">
-          <span>2 000</span>
-          <div className="aProduct">
-          <a href="#pablo">Edit</a>
-          <a href="#pablo" style={{"color":'red'}}>Delete</a>
-          </div>
-        </div>
-      </>
-    ),
-  },
-  {
-    key: "5",
-    name: (
-      <>
-        <Avatar.Group>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJDCfJoArPLNaU2wiv2NcQPvocrWJCV42aBK8LFqy-ADnU3VxdkqBZ9VOCvRyCFjkiu0&usqp=CAU" alt="" className="someone" />
-        </Avatar.Group>{" "}
-      </>
-    ),
-    function: (
-      <>
-        <div className="author-info">
-          <Title level={5}>M200</Title>
- 
-        </div>
-      </>
-    ),
-
-    status: (
-      <>
-              Наименование опции
-      </>
-    ),
-    employed: (
-      <>
-        <div className="ant-employed">
-          <span>2 000</span>
-          <div className="aProduct">
-          <a href="#pablo">Edit</a>
-          <a href="#pablo" style={{"color":'red'}}>Delete</a>
-          </div>
-        </div>
-      </>
-    ),
-  },
-
-  {
-    key: "6",
-    name: (
-      <>
-        <Avatar.Group>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqJDCfJoArPLNaU2wiv2NcQPvocrWJCV42aBK8LFqy-ADnU3VxdkqBZ9VOCvRyCFjkiu0&usqp=CAU" alt="" className="someone" />
-        </Avatar.Group>{" "}
-      </>
-    ),
-    function: (
-      <>
-        <div className="author-info">
-          <Title level={5}>M200</Title>
-        </div>
-      </>
-    ),
-
-    status: (
-      <>
-              Наименование опции
-      </>
-    ),
-    employed: (
-      <>
-        <div className="ant-employed">
-          <span>2 000</span>
-          <div className="aProduct">
-          <a href="#pablo">Edit</a>
-          <a href="#pablo" style={{"color":'red'}}>Delete</a>
-          </div>
-        </div>
-      </>
-    ),
-  },
-];
-// project table start
-
-
-function Tables() {
-  const onChange = (e) => console.log(`radio checked:${e.target.value}`);
 
   return (
     <>
       <div className="tabled">
+        
         <CardsProduct/> <br />  
         <Row gutter={[24, 0]}>
           <Col xs="24" xl={24}>
@@ -307,7 +93,7 @@ function Tables() {
               title="Authors Table"
               extra={
                 <>
-                  <Radio.Group onChange={onChange} defaultValue="a">
+                  <Radio.Group onChange={()=>onChange} defaultValue="a">
                     <Radio.Button value="a">All</Radio.Button>
                     <Radio.Button value="b">ONLINE</Radio.Button>
                   </Radio.Group>
