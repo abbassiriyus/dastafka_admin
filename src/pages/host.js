65094c8eaec4e1ab9e50b27477c97dd1f4889638
abbassiriyus <siriyusabbos@gmail.com>
2023-09-23 14:10:21 +0500

    wer
    wer

--- src/pages/host.js
+++ src/pages/host.js
@@ -1,5 +1,5 @@
-// var url="https://dastafka-back.onrender.com"
-// var ur`l="http://siriyuuc.beget.t`ech"
- var url="http://localhost:5000"
+var url="https://dastafka-back.onrender.com"
+// var url="http://siriyuuc.beget.tech"
+//  var url="http://localhost:5000"
 
 module.exports=url
